637589b4f13db0e5851228bc384ddcf3b78e5a4c
qqchen <785019257@qq.com>
2019-04-05 14:38:46 +0800

    first wordpress

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Comment;
 use App\Http\Requests\PostCommentRequest;
-use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-	public function store(PostCommentRequest $request)
-	{
-		Comment::create(array_merge($request->all(),['user_id'=>\Auth::user()->id]));
-		return redirect()->action('PostsController@show',['id'=>$request->get('discussioin_id')]);
+    public function store(PostCommentRequest $request)
+    {
+        Comment::create(array_merge($request->all(), ['user_id' => \Auth::user()->id]));
+
+        return redirect()->action('PostsController@show', ['id' => $request->get('discussioin_id')]);
     }
 }
